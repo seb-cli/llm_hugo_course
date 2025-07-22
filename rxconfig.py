b2a220184bb842a7f1ede9fa82e3a363ddbb4f7b
Seb <essays_curly.7k@icloud.com>
2025-07-22 17:24:52 +0200

    Add config files

---
+++ rxconfig.py
@@ -0,0 +1,25 @@
+import reflex as rx
+from llm_frontend.style import create_colors_dict
+
+tailwind_config={
+    "darkMode": "class",   
+    "theme": {
+        "colors": {
+            **create_colors_dict(),
+        },
+    },
+}
+
+config = rx.Config(
+    app_name="llm_frontend",    
+    api_url="http://localhost:9000",    
+    backend_port=9000,
+    deployment_name="deployment",    
+    # plugins=[rx.plugins.TailwindV3Plugin()],    
+    # # tailwind=None,
+    plugins=[
+        rx.plugins.TailwindV3Plugin(tailwind_config),
+        rx.plugins.sitemap.SitemapPlugin(),
+    ],
+)
+ 
