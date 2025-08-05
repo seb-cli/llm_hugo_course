ba7c4177685dbe09474c8110345db832f005d4f0
Seb <essays_curly.7k@icloud.com>
2025-08-06 00:05:28 +0200

    Change homepage header

--- word_map/views/templates.py
+++ word_map/views/templates.py
@@ -33,17 +33,17 @@ def template_card(icon: str, title: str, description: str, color: str) -> rx.Com
 
 def templates() -> rx.Component:
     return rx.box(
-        rx.heading("Find Your Future Recruits"),
+        rx.heading("Word Map."),
         rx.image(
             src="/word_map.png",
             height="200px",
             border_radius="150px 150px",
             # class_name="opacity-70 w-auto h-11 pointer-events-none",
         ),
-        # rx.hstack(
-        #    rx.text.kbd("Enter"),
-        #    rx.text("the world of your documents", size="4")
-        # ),
+        rx.hstack(
+           rx.text.kbd("Enter", size="5"),
+           rx.text("the world of your documents", size="4")
+        ),
         rx.box(
             template_card(
                 template_prompts_ddicts["jobs"]["icon"],
